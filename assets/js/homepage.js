13d31ff6ab855862295265b195949967a6f77ec7
bluesatyr <shawnevans.music@gmail.com>
2021-02-01 17:20:23 -0500

    add buttons to search featured repos by language

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -3,6 +3,8 @@ var userFormEl = document.querySelector('#user-form');
 var nameInputEl = document.querySelector('#username');
 var repoContainerEl = document.querySelector('#repos-container');
 var repoSearchTerm = document.querySelector('#repo-search-term');
+var languageButtonsEl = document.querySelector('#language-buttons');
+
 
 var formSubmitHandler = function () {
     event.preventDefault();
@@ -15,6 +17,19 @@ var formSubmitHandler = function () {
     } else {
         alert("Please enter a GitHub username");
     }
+};
+
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language);
+            });
+        } else {
+            alert("Error: " + response.statusText);
+        }
+    });
 };
 
 
@@ -88,6 +103,16 @@ var displayRepos = function(repos, searchTerm){
     console.log(searchTerm);
 };
 
+var buttonClickHandler = function(Event) {
+    var language = event.target.getAttribute("data-language");
+    if (language ) {
+        getFeaturedRepos(language);
+        
+        // clear old content
+        repoContainerEl.textContent = "";
+    }
+};
 
 
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
